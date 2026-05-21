e3d2fc12b79bddeb1285e70b3fcc42f71f58483e
Brendow <helton.do-nascimento-rodrigues-barbosa@eleve.isep.fr>
2026-05-21 16:32:53 +0200

    Changes in this commit: - public/css/admin.css - public/js/dashboard.js - public/views/dashboard.html Creation of new section "My Profile" in dashboard.html, and added user email display in dashboard.js. Updated sidebar and dashboard greetings to show only first name.

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -315,11 +315,38 @@ async function loadUser() {
     document.getElementById('sidebar-name').textContent   = `${user.first_name}`;
     document.getElementById('dash-greeting').textContent  = `${getGreeting()}, ${user.first_name} 👋`;
     document.getElementById('dash-name').innerHTML   = `${user.first_name}`+'👤';
+    document.getElementById('dash-profile').innerHTML   = `${user.first_name}`+' '+' '+`${user.last_name}`;
+    document.getElementById('dash-email').innerHTML   = `${user.email}`;
+
   } catch (err) {
     console.error('[user]', err);
   }
 }
+// ── Infos utilisateur (sidebar) ───────────────────────────
+function upload() {
 
+  const fileUploadInput = document.querySelector('.file-uploader');
+  // using index [0] to take the first file from the array
+  const image = fileUploadInput.files[0];
+
+  // check if the file selected is not an image file
+  if (!image.type.includes('image')) {
+    return alert('Only images are allowed!');
+  }
+
+  // check if size (in bytes) exceeds 10 MB
+  if (image.size > 10_000_000) {
+    return alert('Maximum upload size is 10MB!');
+  }
+   const fileReader = new FileReader();
+    fileReader.readAsDataURL(image);
+
+    fileReader.onload = (fileReaderEvent) => {
+    const profilePicture = document.querySelector('.profile-picture');
+    profilePicture.style.backgroundImage = `url(${fileReaderEvent.target.result})`;
+    
+  }
+}
 // ── Sign out ──────────────────────────────────────────────
 document.getElementById('btn-signout')?.addEventListener('click', async () => {
   try {
